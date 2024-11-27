1b9bef3e7bc9223326a5da4b257102bb4b67eb58
KevinEraso <kerasom@unicauca.edu.co>
2024-11-26 19:57:46 -0500

    mrk

--- server/src/dataProcessing/scripts/script1.py
+++ server/src/dataProcessing/scripts/script1.py
@@ -44,15 +44,11 @@ def analyze_ticket_data(json_data, filters):
 if __name__ == "__main__":
     # Leer los datos JSON de stdin
     input_data = json.load(sys.stdin)
-
     # Leer los filtros enviados en la entrada estándar
     filters = input_data.get('filters', [])
-
     # Eliminar la clave 'filters' del input_data antes de procesar
     input_data = input_data.get('data', input_data)
-
     # Procesar los datos
     output_data = analyze_ticket_data(input_data, filters)
-
     # Escribir el resultado en formato JSON en stdout
     print(json.dumps(output_data, indent=4))
